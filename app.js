21d124a17af5e6bc359cd0f73aa82d2476bbd8f0
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-05 16:26:09 -0300

    addfunction carregaListaDespesas()

--- app.js
+++ app.js
@@ -43,6 +43,29 @@ class Bd {
     localStorage.setItem(id, JSON.stringify(d))
     localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        let despesas = Array ()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for (let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+            
+            //existe a possibilidade de haver índices que foram pulados/removidos
+            //nestes cass nós vamos pular esses índices
+            if(despesa === null) {
+                continue
+            }
+
+            //coloca todas as despesas dentro de um array
+            despesas.push(despesa)
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -66,7 +89,7 @@ function cadastrarDespesa() {
         )
 
         if(despesa.validarDados()) {
-        //bd.gravar(despesa)
+        bd.gravar(despesa)
         
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
@@ -79,11 +102,49 @@ function cadastrarDespesa() {
         
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro.'
         document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
-        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifque se todos os campos foram preenchidos corretamente!'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
         document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
         document.getElementById('modal_btn').className = 'btn btn-danger'
         //dialog de erro
         $('#modalRegistraDespesa').modal('show')
     }
 
+}
+
+function carregaListaDespesas() {
+
+    let despesas = Array ()
+    despesas = bd.recuperarTodosRegistros() 
+
+    console.log(despesas)
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d) {
+
+        // criando a lin (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+
+        //ajustar o tipo 
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
